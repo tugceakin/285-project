5be3dcc1fc4a19d8d9716183b8f7473ebc10ca4b
Pratik Vyas <m@pd.io>
2017-05-20 11:44:08 -0700

    add index investing strategy

--- init_db.py
+++ init_db.py
@@ -11,9 +11,14 @@ print 'creating symbols'
 with open('stocks.txt', 'r') as f:
     for line in f:
         db.session.add(Symbol(symbol=line.strip(), symbol_type='STOCK'))
+
 with open('etfs.txt', 'r') as f:
     for line in f:
         db.session.add(Symbol(symbol=line.strip(), symbol_type='ETF'))
 
+with open('index.txt', 'r') as f:
+    for line in f:
+        db.session.add(Symbol(symbol=line.strip(), symbol_type='ETF', index=True))
+
 for a in Symbol.query.all():
-    print a.id, a.symbol, a.symbol_type
+    print a.id, a.symbol, a.symbol_type, a.index
